1d8bc33cd3679e58e42446193d97aa0a92f640a0
Arthur Candido <oarthurcandido@gmail.com>
2023-09-16 09:20:20 -0300

    :tada: first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,175 @@
-import Image from 'next/image'
+"use client";
+import Button from "@/components/Button";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+interface Question {
+  id: number;
+  materia: string;
+  pergunta: string;
+  respostas: string[];
+  respostaCorreta: string;
+  fundamento?: string | undefined;
+}
+
+interface Disciplinas {
+  [edd: string]: string | undefined;
+  fpc: string;
+  sc: string;
+}
+
+const disciplinas: Disciplinas = {
+  edd: "Estrutura de dados",
+  fpc: "Formação profssional em computação",
+  sc: "Sistemas computacionais",
+};
 
 export default function Home() {
+  const [theme, setTheme] = useState<string>("empty");
+  const [questions, setQuestions] = useState<Question[]>([]);
+  const [shuffledQuestions, setShuffledQuestions] = useState<Question[]>([]);
+  const [points, setPoints] = useState<number>(0);
+  const [round, setRound] = useState<number>(1);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [isRight, setIsRight] = useState<boolean>(false);
+  const [isWrong, setIsWrong] = useState<boolean>(false);
+  const length = shuffledQuestions.length;
+
+  useEffect(() => {
+    async function load() {
+      setLoading(true);
+      const questions = await fetch(
+        "https://65023949736d26322f5ce1f6.mockapi.io/questions"
+      );
+      const data = await questions.json();
+      setQuestions(data);
+      setLoading(false);
+    }
+    load();
+  }, []);
+
+  useEffect(() => {
+    if (theme === "empty") return;
+    else {
+      const filtered = questions.filter((item) => item.materia === theme);
+      const shuffled = filtered.sort(() => 0.5 - Math.random());
+      setShuffledQuestions(shuffled);
+    }
+  }, [theme, questions]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="max-w-lg mx-1 sm:mx-auto [&_div]:border-gray-500">
+      <header className="border rounded-md p-1 flex justify-between border-gray-500">
+        <p className="text-2xl font-extrabold">Quiz</p>
+        <p className="text-2xl font-extrabold">Pontos: {points}</p>
+      </header>
+      <section className="mt-2">
+        <div className="border rounded-md p-1">
+          {loading ? (
+            <p>Carregando perguntas...</p>
+          ) : (
+            <p className="text-xl font-extrabold">
+              {" "}
+              {theme == "empty"
+                ? "Bem vindo ao Quiz, escolha a disciplina que quer jogar."
+                : `${disciplinas[theme]} - Faltam  ${length} perguntas`}
+            </p>
+          )}
         </div>
-      </div>
+        {theme === "empty" && (
+          <div id="themeSelector" className="my-1 border rounded-md p-1">
+            <Button
+              onClick={() => setTheme("edd")}
+              key={"edd"}
+              color="attention"
+              title="Estrutura de dados"
+            />
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+            <Button
+              aria-label="Formação profssional em computação"
+              key={"fpc"}
+              onClick={() => setTheme("fpc")}
+              color="primary"
+              title="Formação profssional em computação"
+            />
+            {/* <Button
+              key={"sc"}
+              onClick={() => setTheme("sc")}
+              color="tertiary"
+              title="Sistemas computacionais"
+            /> */}
+          </div>
+        )}
+      </section>
+      <section id="game-box">
+        {shuffledQuestions.length > 0 && !isRight && !isWrong && (
+          <div className="border rounded-md p-1 mt-2">
+            <p className="sm:text-xl font-bold ">
+              {shuffledQuestions[0].pergunta}
+            </p>
+            <div className="my-1 border rounded-md p-1">
+              {shuffledQuestions[0].respostas.map((item, index) => (
+                <Button
+                  key={index}
+                  onClick={() => {
+                    if (item === shuffledQuestions[0].respostaCorreta) {
+                      setIsRight(true);
+                      setPoints(points + 1);
+                      setRound(round + 1);
+                    } else {
+                      setIsWrong(true);
+                      setRound(round + 1);
+                    }
+                  }}
+                  color="basic"
+                  title={item}
+                />
+              ))}
+            </div>
+          </div>
+        )}
+        {isRight && (
+          <div className="flex flex-col items-center">
+            <p className="sm:text-xl font-bold">Resposta certa!</p>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+            <Button
+              title="OK"
+              color="primary"
+              onClick={() => {
+                setIsRight(false);
+                const newQuestions = shuffledQuestions.slice(1);
+                setShuffledQuestions(newQuestions);
+              }}
+            ></Button>
+          </div>
+        )}
+        {isWrong && (
+          <div className="flex flex-col items-center">
+            <p className="sm:text-xl font-bold">Você errou</p>
+            {/* {shuffledQuestions[0].fundamento && (
+              <div className="border rounded-md p-1">
+                <p className="sm:text-xl font-bold ">
+                  {shuffledQuestions[0].fundamento}
+                </p>
+              </div>
+            )} */}
+            <Button
+              title="Responder novamente"
+              color="tertiary"
+              onClick={() => setIsWrong(false)}
+            ></Button>
+            <Button
+              title="OK"
+              color="attention"
+              onClick={() => {
+                setIsWrong(false);
+                const newQuestions = shuffledQuestions.slice(1);
+                setShuffledQuestions(newQuestions);
+              }}
+            ></Button>
+          </div>
+        )}
+      </section>
     </main>
-  )
+  );
 }
---
+++ src/components/Button.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import CN from "classnames";
+
+interface ButtonProps {
+  title?: string;
+  color?: "attention" | "primary" | "tertiary" | "basic" | undefined;
+  onClick?: () => void;
+  children?: React.ReactNode;
+}
+
+const Button: React.FC<ButtonProps> = ({
+  title,
+  color,
+  children,
+  ...props
+}) => {
+  return (
+    <button
+      {...props}
+      className={CN(
+        `rounded-md border p-1 my-1 hover:opacity-60 cursor-pointer w-full text-white text-sm sm:text-base`,
+        {
+          "bg-red-600": color === "attention",
+          "bg-green-600": color === "primary",
+          "bg-yellow-500": color === "tertiary",
+          "bg-slate-500": color === "basic",
+        }
+      )}
+    >
+      {title}
+      {children}
+    </button>
+  );
+};
+
+export default Button;
+//
